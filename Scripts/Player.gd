bf4220d7b9766f3ed45aadc265a5feeb68117d09
mugem123 <20silva.s20@gmail.com>
2025-04-26 14:17:18 -0300

    Alterando as animações do player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,6 +1,5 @@
 extends KinematicBody2D
 
-# Teste
 var velocity = Vector2.ZERO;
 var move_speed = 480;
 var gravity = 1200;
@@ -17,10 +16,14 @@ func _physics_process(delta: float) -> void:
 	velocity = move_and_slide(velocity)
 	
 	is_grounded = _check_is_ground()
+	
+	_set_animation()
 
 func _get_input():
+	velocity.x = 0
 	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"));
-	velocity.x = move_speed * move_direction;
+	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2)
+
 	
 	if move_direction != 0:
 		$texture.scale.x = move_direction
@@ -36,3 +39,12 @@ func _check_is_ground():
 			
 	return false
 	
+func _set_animation():
+	var anim = "idle"
+	if !is_grounded:
+		anim = "jump"
+	elif velocity.x != 0:
+		anim = "run"
+	
+	if $anim.assigned_animation != anim:
+		$anim.play(anim)
