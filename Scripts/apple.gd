c97ce1fb7d0856cc46a3fe3a40dd8c9e87fbb2d2
mugem123 <20silva.s20@gmail.com>
2025-05-31 15:05:10 -0300

    Aula coletáveis 2

--- Scripts/apple.gd
+++ Scripts/apple.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends Area2D
 
 
 func _ready():
@@ -8,3 +8,8 @@ func _ready():
 
 func _on_items_body_entered(body):
 	print(body.name)
+	$anim.play("collected")
+
+func _on_anim_animation_finished(anim_name):
+	if anim_name == "collected":
+		queue_free()
