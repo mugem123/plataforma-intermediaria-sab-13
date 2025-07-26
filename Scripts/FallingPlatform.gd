84a5db32c7ad09e7801111ae584720f29858eee2
mugem123 <20silva.@gmail.com>
2025-07-26 15:02:03 -0300

    commit aula plataforma que cai (movimento)

--- Scripts/FallingPlatform.gd
+++ Scripts/FallingPlatform.gd
@@ -16,3 +16,23 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	position += velocity * delta
+	
+func collide_with(_collision: KinematicCollision2D, _collider: KinematicBody2D):
+	if !is_triggered:
+		is_triggered = true
+		anim.play("shake")
+		velocity = Vector2.ZERO
+func _on_anim_animation_finished(anim_name):
+	set_physics_process(true)
+	timer.start(reset_timer)
+
+
+func _on_timer_timeout():
+	set_physics_process(false)
+	yield(get_tree(), "physics_frame")
+	var temp = collision_layer
+	collision_layer = 0
+	global_position = reset_position
+	yield(get_tree(), "physics_frame")
+	collision_layer = temp
+	is_triggered = false
